c1a45566adec13c49750bacac4eb5bbbc2c01bdd
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2023-01-12 11:52:46 -0700

    in class demo. introduction to new project

---
+++ Movies/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Models;
+
+namespace Movies.Controllers
+{
+    public class MovieController : Controller
+    {
+        private static List<Movie> MoviesList = new List<Movie>
+        {
+            new Movie("Hunger Games", 2012, 4.5f),
+            new Movie("Saving Private Ryan", 1998, 5.0f),
+            new Movie("Woknda Forever", 2022, 4.7f)
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayMovie()
+        {
+            Movie m = new Movie("Iron Man", 2008, 5.0f);
+            return View(m);
+        }
+
+        public IActionResult MultiMovies()
+        {
+
+            return View(MoviesList);
+        }
+    }
+}
---
+++ Movies/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿namespace Movies.Models
+{
+    public class Movie
+    {
+        private static int? nextID = 0;
+        public int? Id { get; set; } = nextID++;
+        public string title { get; set; } = "[No Title]";
+        public int? year { get; set; } = 1930;
+        public float? rating { get; set; } = 0.0f;
+        public DateTime? date { get; set; }
+        public string image { get; set; }
+
+        public Movie() { }
+
+        public Movie(string strTitle, int intYear, float fltRating)
+        {
+            this.title = strTitle;
+            this.year = intYear;
+            this.rating = fltRating;
+        }
+
+        public override string ToString()
+        {
+            return $"{title} - {year} - {rating} stars";
+        }
+    }
+}
