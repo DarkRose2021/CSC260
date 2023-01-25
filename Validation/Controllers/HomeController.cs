bbcecc9b67fd1d4b64a41782f82f1a645b66249f
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2023-01-25 13:50:10 -0700

    Started and finished the Validation exercise

---
+++ Validation/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Validation.Models;
+
+namespace Validation.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public static List<Profile> profileList = new List<Profile>();
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Profile p)
+		{
+			if (ModelState.IsValid)
+			{
+				profileList.Add(p);
+				TempData["success"] = "Profile added";
+				return RedirectToAction("ViewProfile", "Home");
+			}
+			return View();
+		}
+
+		public IActionResult ViewProfile()
+		{
+			return View(profileList);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Validation/Models/Profile.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Validation.Models
+{
+	public class Profile
+	{
+		[Required(ErrorMessage = "You must enter a name")]
+		public string? Name { get; set; }
+
+		[Required]
+		[Range(5, 130, ErrorMessage = "Age must between 5 and 130")]
+		public int? Age { get; set; }
+
+		[Required(ErrorMessage = "You can't leave this field empty")]
+		public string? street { get; set; }
+
+		[Required(ErrorMessage = "You must enter a city")]
+		public string? city { get; set; }
+
+		[Required(ErrorMessage = "You must enter a state")]
+		public string? state { get; set; }
+
+		[Required]
+		[MinLength(5, ErrorMessage = "US zip codes are a minimum of 5 numbers")]
+		[MaxLength(9, ErrorMessage = "US zip codes are a maximum of 9 numbers")]
+		public int? zipcode { get; set; }
+	}
+}
